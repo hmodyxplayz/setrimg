0bd5cd00784aadb20aab15753dce87219499fa4c
hmodyxplayz <45116597+hmodyxplayz@users.noreply.github.com>
2018-11-24 17:49:53 +0300

    Update bc.js

--- bc.js
+++ bc.js
@@ -15,10 +15,10 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : 3zozHuler `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`الكلام الي تبيه في ستريمينق`,"http://twitch.tv/S-F")
+client.user.setGame(`اtest`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
 
 
- client.login("تىكن حسابك");
+ client.login("MzA3MTg3MTA2OTkwNjUzNDQx.Dtr1IQ.nH4BCTD98PLFvBNAWzEPCo5oGq0");
